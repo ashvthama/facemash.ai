c6a6a695838dbe0b20e4181cb4355ca9e6a56c35
Kedar Bargule <kedarbargule@gmail.com>
2025-09-06 18:45:30 +0530

    Authentication UI

---
+++ src/lib/auth.ts
@@ -0,0 +1,17 @@
+import { db } from "@/db";
+import * as schema from "@/db/schema";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+// Se pasa el schema personal creado a drizzleAdapter para que se pueda usar con betterAuth
+export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: {
+      ...schema,
+    },
+  }),
+});
